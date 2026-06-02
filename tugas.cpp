3cbb3694a384771ecf6126f9ad5322f9f5579992
rmezzeluna-png <rmezzeluna@gmail.com>
2026-06-02 16:44:21 +0700

    public

--- tugas.cpp
+++ tugas.cpp
@@ -10,3 +10,12 @@ using namespace std;
 class TokoElektronik {
 private:
     array<string, 3> etalase;
+
+
+public:
+    // Constructor
+    TokoElektronik() {
+        etalase[0] = "Laptop ASUS";
+        etalase[1] = "Smartphone Samsung";
+        etalase[2] = "Printer Epson";
+    }
