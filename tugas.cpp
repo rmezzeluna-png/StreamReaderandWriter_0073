a30a4c2e615b165173353434b58dd77fbaa61b47
rmezzeluna-png <rmezzeluna@gmail.com>
2026-06-02 16:44:51 +0700

    method try catch

--- tugas.cpp
+++ tugas.cpp
@@ -19,3 +19,17 @@ public:
         etalase[1] = "Smartphone Samsung";
         etalase[2] = "Printer Epson";
     }
+
+    // Method mengambil produk
+    string ambilProduk(size_t nomorRak) {
+        try {
+            return etalase.at(nomorRak);
+        }
+        catch (const out_of_range&) {
+            throw string("Gagal Mengambil Barang : Rak nomor " +
+                         to_string(nomorRak) +
+                         " kosong atau tidak tersedia!");
+        }
+    }
+};
+
